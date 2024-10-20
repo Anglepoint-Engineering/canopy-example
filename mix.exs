4403fb14fe85baaeeedb5619fdc5bb35488f384f
Thomas Silva <thomas.silva@anglepoint.com>
2024-10-19 20:41:44 -0600

    github-test-13

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule CanopyExample.MixProject do
   defp deps do
     [
       {:canopy,
-       git: "https://github.com/Anglepoint-Engineering/canopy.git", tag: "github-test-12"}
+       git: "https://github.com/Anglepoint-Engineering/canopy.git", tag: "github-test-13"}
     ]
   end
 end
